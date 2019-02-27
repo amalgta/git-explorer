d8cd1152cc612c394de2e9e0a58268e26e1c4a99
AMAL, THOMAS <amal.thomas@nissanmotor.com>
2019-02-27 11:27:09 +0530

    Basic HTTP Request in Cordova.

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/studio/styx/mobile/GitExplorer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package studio.styx.mobile.GitExplorer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "studio.styx.mobile.GitExplorer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0";
+}
